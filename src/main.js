f5888d2edb853e426b19a491381785d718cbf517
Andael <1176969+Andael@users.noreply.github.com>
2022-09-17 11:55:39 +1200

    Instant Token Preview (#1)

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import log from './utils/log.js'
+import * as instantTokenPreview from './aspects/instant-token-preview.js'
 
 Hooks.on('ready', function()
 {
+    instantTokenPreview.activate()
     log('Ready')
 })
--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -1,7 +1,25 @@
 interface Hooks {
     ready: () => void
+    renderTokenConfig: (app: unknown, html: JQuery, options: unknown) => void
 }
 
 declare const Hooks: {
     on<K extends keyof Hooks>(name: K, fn: Hooks[K]): void
+}
+
+interface JQuery {
+    on<K extends keyof HTMLElementEventMap>(
+        eventName: K,
+        listener: (this: unknown, ev: HTMLElementEventMap[K]) => any): this;
+    siblings(selector: string): JQuery
+    trigger<K extends keyof HTMLElementEventMap>(eventName: K): this
+    val(value: string): this
+}
+
+declare const $: {
+    (element: HTMLElement): JQuery
+}
+
+declare namespace foundry.data.validators {
+    export function isColorString(str: string): boolean
 }
