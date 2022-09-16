ba5e00324de38ab4a442a63a1112aa091d682d0f
Andael <1176969+Andael@users.noreply.github.com>
2022-09-17 10:28:19 +1200

    Add docos and types

--- src/aspects/more-interactive-fields.js
+++ src/aspects/more-interactive-fields.js
@@ -1,26 +1,42 @@
 /**
- * @file
- * This module makes input fields update more interactively.
+ * @file Instant Token Preview
+ * Foundry V10 lets you preview any changes you make to a token’s configuration. However, the
+ * preview only updates on blur (for text fields) or when you let go (for sliders). This feature
+ * makes it so that the preview updates immediately.
  */
 
 export function activate()
 {
     Hooks.on('renderTokenConfig', function(_, form)
     {
-        form.on('input', function(evt)
-        {
-            const input = evt.target
-            if (!(input instanceof HTMLInputElement))
-                return
-
-            if (input.type == 'text' && input.classList.contains('color'))
-            {
-                if (!foundry.data.validators.isColorString(input.value))
-                    return
-                $(input).siblings('[type=color]').val(input.value)
-            }
-
-            $(input).trigger('change')
-        })
+        form.on('input', onFormInput)
     })
+}
+
+/**
+ * Called when the [input](https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/input_event)
+ * event is triggered inside a TokenConfig dialog.
+ * @param {Event} evt
+ */
+function onFormInput(evt)
+{
+    const input = evt.target
+
+    // Ignore select/textarea fields:
+    if (!(input instanceof HTMLInputElement))
+        return
+
+    // Text fields used to enter colors require some special logic:
+    if (input.type == 'text' && input.classList.contains('color'))
+    {
+        // Ignore incomplete text (let the user finish typing):
+        if (!foundry.data.validators.isColorString(input.value))
+            return
+
+        // Update the associated color picker:
+        $(input).siblings('[type=color]').val(input.value)
+    }
+
+    // Trick Foundry into updating the preview:
+    $(input).trigger('change')
 }
