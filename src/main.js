92c266a283580edeaaa2eeebb45e04c24de1736c
Andael <1176969+Andael@users.noreply.github.com>
2022-09-17 11:50:01 +1200

    remove unrelated things

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import log from './utils/log.js'
-import * as instantTokenPreview from './aspects/instant-token-preview.js'
 
 Hooks.on('ready', function()
 {
-    instantTokenPreview.activate()
     log('Ready')
 })
