a5896571d02017a22277d6dfacb369a3dfe49064
Andael <1176969+Andael@users.noreply.github.com>
2022-09-17 15:28:31 +1200

    remove unrelated changes

--- src/main.js
+++ src/main.js
@@ -1,16 +1,9 @@
 import log from './utils/log.js'
-import * as imageOffset from './aspects/image-offset.js'
 import * as instantTokenPreview from './aspects/instant-token-preview.js'
 
 Hooks.on('renderTokenConfig', function(app, html)
 {
-    imageOffset.renderTokenConfig(app, html)
     instantTokenPreview.enable(html)
-})
-
-Hooks.on('refreshToken', function(token)
-{
-    imageOffset.refreshToken(token)
 })
 
 Hooks.on('ready', function()
